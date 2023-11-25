0e79318751f2e290c27245b72ddc0d9a96b98d2a
takashabe <i@takashabe.dev>
2023-11-25 17:14:47 +0900

    remote

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -8,7 +8,7 @@ cat /var/log/nginx/access.log | alp ltsv --sort avg -r -m '/api/user/.+/icon','a
 head -n 30 $ALP_FILENAME | notify_slack
 
 ## mysql
-echo "set global slow_query_log = OFF;" | mysql -h192.168.0.13 -uisucon -pisucon isupipe
+# echo "set global slow_query_log = OFF;" | mysql -h192.168.0.13 -uisucon -pisucon isupipe
 scp isucon@192.168.0.13:/var/log/mysql/slow.log /var/log/mysql/slow.log
 export QUERY_FILENAME=/tmp/query-`date +%H%M%S`
 pt-query-digest /var/log/mysql/slow.log > $QUERY_FILENAME
--- bin/bench_ready.sh
+++ bin/bench_ready.sh
@@ -6,6 +6,6 @@ ssh isucon@192.168.0.13 "sudo sh -c 'echo > /var/log/mysql/slow.log'"
 # :> /var/log/mysql/slow.log
 
 ## require change to connect mysql
-echo "set global slow_query_log_file = '/var/log/mysql/slow.log';set global long_query_time=0;set global slow_query_log = ON;" | mysql -h192.168.0.13 -uisucon -pisucon isupipe
+# echo "set global slow_query_log_file = '/var/log/mysql/slow.log';set global long_query_time=0;set global slow_query_log = ON;" | mysql -h192.168.0.13 -uisucon -pisucon isupipe
 
 restart_app.sh

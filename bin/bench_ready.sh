235abaadac839d9263ddd63cd745b016454dce29
takashabe <i@takashabe.dev>
2023-11-25 17:17:13 +0900

    file

--- bin/bench_ready.sh
+++ bin/bench_ready.sh
@@ -2,7 +2,7 @@
 
 :> /var/log/nginx/access.log
 
-ssh isucon@192.168.0.13 "sudo sh -c 'echo > /var/log/mysql/slow.log'"
+ssh isucon@192.168.0.13 "sudo sh -c 'echo > /var/log/mysql/mysql-slow.log'"
 # :> /var/log/mysql/slow.log
 
 ## require change to connect mysql

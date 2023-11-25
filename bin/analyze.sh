91105f534b4546290e84f6df300c5c4a50ddb8f3
takashabe <i@takashabe.dev>
2023-11-25 17:17:41 +0900

    slow

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -9,7 +9,7 @@ head -n 30 $ALP_FILENAME | notify_slack
 
 ## mysql
 # echo "set global slow_query_log = OFF;" | mysql -h192.168.0.13 -uisucon -pisucon isupipe
-scp isucon@192.168.0.13:/var/log/mysql/slow.log /var/log/mysql/slow.log
+scp isucon@192.168.0.13:/var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log
 export QUERY_FILENAME=/tmp/query-`date +%H%M%S`
-pt-query-digest /var/log/mysql/slow.log > $QUERY_FILENAME
+pt-query-digest /var/log/mysql/mysql-slow.log > $QUERY_FILENAME
 head -n 30 $QUERY_FILENAME | notify_slack

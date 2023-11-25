182d668085d07966540e1ac2b0a416d0fd2f6b86
takashabe <i@takashabe.dev>
2023-11-25 12:17:07 +0900

    index

--- sql/init.sh
+++ sql/init.sh
@@ -68,6 +68,11 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < initial_livecomments.sql
 
-bash ../pdns/init_zone.sh 
+bash ../pdns/init_zone.sh
 
-
+# 色々チューニングする
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < isu.sql

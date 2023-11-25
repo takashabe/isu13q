872ebf1ea3ebc1cb18a5e0d99e757a6755700598
takashabe <i@takashabe.dev>
2023-11-25 14:03:02 +0900

    index

--- sql/initdb.d/10_schema.sql
+++ sql/initdb.d/10_schema.sql
@@ -111,3 +111,4 @@ create index icons_user_id_idx on icons (user_id);
 create index themes_user_id_idx on themes (user_id);
 create index ng_words_user_id_livestream_id_idx on ng_words (user_id, livestream_id);
 create index resevation_slots_start_at_end_at_idx on reservation_slots (start_at, end_at);
+create index livecomments_livecomment_id_idx on livecomment_reports (livecomment_id);

5bab0214f16d4e01e330ac634d4c8c85fd220cf3
takashabe <i@takashabe.dev>
2023-11-25 16:55:15 +0900

    remote db

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -8,7 +8,8 @@ cat /var/log/nginx/access.log | alp json --sort avg -r > $ALP_FILENAME
 head -n 30 $ALP_FILENAME | notify_slack
 
 ## mysql
-echo "set global slow_query_log = OFF;" | mysql -uisucon -pisucon isupipe
+echo "set global slow_query_log = OFF;" | mysql -h192.168.0.13 -uisucon -pisucon isupipe
+scp isucon@192.168.0.13:/var/log/mysql/slow.log /var/log/mysql/slow.log
 export QUERY_FILENAME=/tmp/query-`date +%H%M%S`
 pt-query-digest /var/log/mysql/slow.log > $QUERY_FILENAME
 head -n 30 $QUERY_FILENAME | notify_slack

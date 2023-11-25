ccff27694c272678b2f9913896e66c3c10df8dc8
takashabe <i@takashabe.dev>
2023-11-25 12:23:08 +0900

    index

--- sql/isu.sql
+++ sql/isu.sql
@@ -1,11 +1,6 @@
-create index livestream_tags_livestream_id_idx on livestream_tags (livestream_id);
-
-create index livestream_user_id_idx on livestreams (user_id);
-
-create index icons_user_id_idx on icons (user_id);
-
-create index themes_user_id_idx on themes (user_id);
-
-create index ng_words_user_id_livestream_id_idx on ng_words (user_id, livestream_id);
-
-create index resevation_slots_start_at_end_at_idx on reservation_slots (start_at, end_at);
+create index if not exists livestream_tags_livestream_id_idx on livestream_tags (livestream_id);
+create index if not exists livestream_user_id_idx on livestreams (user_id);
+create index if not exists icons_user_id_idx on icons (user_id);
+create index if not exists themes_user_id_idx on themes (user_id);
+create index if not exists ng_words_user_id_livestream_id_idx on ng_words (user_id, livestream_id);
+create index if not exists resevation_slots_start_at_end_at_idx on reservation_slots (start_at, end_at);
